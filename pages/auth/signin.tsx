a4484de1b9cb28e460fdfd94abb4653f03b6586d
Franz Lagler <franz.lagler@outlook.com>
2021-11-29 20:28:17 +0100

    Change login

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -45,8 +45,7 @@ export default function SignIn() {
     if (res.ok) {
       dispatch(logIn());
 
-      router.push('/');
-      router.reload();
+      router.push('/profile');
     } else {
       const message = await res.json();
 
