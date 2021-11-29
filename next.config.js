ef2fea862cfaa0e91266f54cac0ef0284bae2a3e
Franz Lagler <franz.lagler@outlook.com>
2021-11-29 22:51:19 +0100

    Remove next-image

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   reactStrictMode: true,
 };
-const withImages = require('next-images');
-module.exports = withImages();
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
