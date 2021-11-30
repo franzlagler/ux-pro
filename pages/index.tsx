cb1288552cb90c2f7d470d6ba7c409339cb5d0de
Franz Lagler <franz.lagler@outlook.com>
2021-11-30 10:38:49 +0100

    New login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { GetServerSidePropsContext, NextPageContext } from 'next';
+import { GetServerSidePropsContext } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import nookies from 'nookies';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { DropdownButton, LinkButton, LinkLink } from '../components/Buttons';
